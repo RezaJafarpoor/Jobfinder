79a066b44a9444ddc99079f293856c8bd96e36eb
reza <rezajafarpoor222@gmail.com>
2025-03-25 21:34:58 +0330

    jwt configured

--- Jobfinder.Infrastructure/Extensions/ServiceCollectionExtension.cs
+++ Jobfinder.Infrastructure/Extensions/ServiceCollectionExtension.cs
@@ -1,7 +1,13 @@
-﻿using Jobfinder.Infrastructure.Persistence;
+﻿using Jobfinder.Infrastructure.Identity;
+using Jobfinder.Infrastructure.Persistence;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System.Text.Unicode;
 
 namespace Jobfinder.Infrastructure.Extensions;
 
@@ -14,5 +20,36 @@ public static class ServiceCollectionExtension
             option.UseSqlServer(configuration.GetConnectionString("AppDbConnection"))
                 .EnableSensitiveDataLogging();   // WARNING: Remove this this line in production
         });
+    }
+
+    public static void AddIdentity(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddOptions<JwtOption>()
+            .Bind(config: configuration.GetSection("JwtSetting"));
+        
+        services.AddAuthorization();
+        services.AddAuthentication(option =>
+            {
+                option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(option =>
+            {
+                var jwtOption = configuration.GetSection("JwtSetting").Get<JwtOption>();
+                if (jwtOption is null)
+                {
+                    throw new Exception("Something is wrong with Jwt Settings");
+                }
+                option.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidateLifetime = true,
+                    RequireExpirationTime = true,
+                    ClockSkew = TimeSpan.FromMinutes(jwtOption.ExpirationTimeInMinute),
+                    ValidIssuer = jwtOption.Issuer,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtOption.Secret))
+                };
+            });
     }
 }
---
+++ Jobfinder.Infrastructure/Identity/JwtOption.cs
@@ -0,0 +1,9 @@
+﻿namespace Jobfinder.Infrastructure.Identity;
+
+public record JwtOption
+{
+    public required string Secret { get; set; } 
+    public required string Issuer { get; set; }
+    public required int ExpirationTimeInMinute { get; set; }
+    
+}

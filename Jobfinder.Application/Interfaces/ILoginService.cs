7f894a9088bb2ffe35a55e84bee0b03d4fd2f750
reza <rezajafarpoor222@gmail.com>
2025-03-27 02:04:24 +0330

    Login service implemented

---
+++ Jobfinder.Application/Interfaces/ILoginService.cs
@@ -0,0 +1,11 @@
+﻿using Jobfinder.Application.Common.Models;
+using Jobfinder.Domain.Dtos;
+
+namespace Jobfinder.Application.Interfaces;
+
+public interface ILoginService
+{
+    Task<Response<TokenResponse>> Login(LoginDto login, CancellationToken cancellationToken);
+
+
+}
---
+++ Jobfinder.Domain/Dtos/LoginDto.cs
@@ -0,0 +1,3 @@
+﻿namespace Jobfinder.Domain.Dtos;
+
+public record LoginDto(string Username, string Password);
